ac031a4fd73e8df16c38e83443df24fc0dfab812
Seth Rodgers <13446321+sethrodg@users.noreply.github.com>
2022-09-24 12:07:33 -0500

    Start database work

---
+++ src/Database/database.js
@@ -0,0 +1,10 @@
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: "",
+    database: "",
+    user: "root",
+    password: "rootuser"
+});
+
+// use info from mysql server configuration 
